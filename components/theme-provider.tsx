a5af1b3143fcc606069029d45cecb3d651142e4f
nocturneFlow <7evenxheaven@gmail.com>
2023-12-25 02:40:14 +0600

    fix: changed speed & size of flakes

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -8,7 +8,12 @@ import Snowfall from "react-snowfall";
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <Snowfall />
+      <Snowfall
+        snowflakeCount={50}
+        speed={[1.0, 1.0]}
+        radius={[0.8, 2.0]}
+        color="#b8b8b8"
+      />
       {children}
     </NextThemesProvider>
   );
