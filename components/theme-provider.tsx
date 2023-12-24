52242005446361593b8a4b1e8ccd25c7575320ba
nocturneFlow <7evenxheaven@gmail.com>
2023-12-25 02:43:25 +0600

    fix: speed changed

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -10,8 +10,8 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     <NextThemesProvider {...props}>
       <Snowfall
         snowflakeCount={50}
-        speed={[1.0, 1.0]}
-        radius={[0.8, 2.0]}
+        speed={[0.8, 1.3]}
+        radius={[0.8, 2.3]}
         color="#b8b8b8"
       />
       {children}
